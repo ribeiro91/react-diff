39e43809c222ff7370afaceeb2a766dc9a9ff6f8
Ribeiro Kishor <ribeirokishor12@gmail.com>
2019-12-31 00:09:31 +0000

    github config changes

---
+++ precache-manifest.06250659d118ea26689fba1e649bdf04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72727b4e3b65dc03553ca9129f0a321",
+    "url": "/react-diff/index.html"
+  },
+  {
+    "revision": "af51ae63341409231ddf",
+    "url": "/react-diff/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9fd10c4ae1a0b96c4ad2",
+    "url": "/react-diff/static/css/main.9ca9a804.chunk.css"
+  },
+  {
+    "revision": "af51ae63341409231ddf",
+    "url": "/react-diff/static/js/2.4199a836.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-diff/static/js/2.4199a836.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fd10c4ae1a0b96c4ad2",
+    "url": "/react-diff/static/js/main.b2c0b274.chunk.js"
+  },
+  {
+    "revision": "6201b5650a3ad5ee7a1b",
+    "url": "/react-diff/static/js/runtime-main.fde1e1e3.js"
+  }
+]);
